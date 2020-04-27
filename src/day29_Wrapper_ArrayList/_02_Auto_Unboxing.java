98ec690709f0a6bffde973ddf68572b550b30c7a
Emre Yilmaz <emreyilmaz1992@outlook.com>
2020-04-27 10:41:58 -0400

    AutoBoxing

---
+++ src/day29_Wrapper_ArrayList/_02_Auto_Unboxing.java
@@ -0,0 +1,24 @@
+package day29_Wrapper_ArrayList;
+
+public class _02_Auto_Unboxing {
+    public static void main(String[] args) {
+
+        int a =100;
+        Integer b = a;  //this is called autoboxing
+
+
+
+        Byte byte1 = 98;
+        short short1 = byte1;  //unboxing
+        byte b2 = byte1;       //unboxing
+        int int1 = byte1;      //unboxing
+
+
+
+
+
+
+
+
+    }
+}
