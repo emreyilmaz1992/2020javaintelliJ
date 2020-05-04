3ed5b47446f7c28159f11c6dc6068522ce08cf78
Emre Yilmaz <emreyilmaz1992@outlook.com>
2020-05-04 14:59:32 -0400

    Custom Class

---
+++ src/day33_Custom_Class/_02_CarObjects.java
@@ -0,0 +1,62 @@
+package day33_Custom_Class;
+
+public class _02_CarObjects {
+
+
+
+    public static void main(String[] args) {
+
+
+        Car car1 = new Car();
+
+        car1.brand = "BMW";
+        car1.model = "X5";
+        car1.year = 2020;
+        car1.color = "Blue";
+
+        System.out.println(car1.brand);   // instance variable are object variable, you can call them through the object name
+        System.out.println(car1.model);
+        System.out.println(car1.year);
+        System.out.println(car1.color);
+
+        car1.start(); // BMW is started
+        car1.drive(); // Driving BMW
+
+        System.out.println("======================================");
+
+        Car car2 = new Car();
+
+
+        car2.brand = "Mercedes";
+        car2.model = "S500";
+        car2.year = 2020;
+        car2.color = "Black";
+
+        System.out.println(car2.brand);
+        System.out.println(car2.model);
+        System.out.println(car2.year);
+        System.out.println(car2.color);
+
+        car2.start(); //  Mercedes is started
+        car2.drive(); //  Driving Mercedes
+        car2.getCarInfo();
+
+        System.out.println("============================");
+
+        Car[] cars = {car1,car2};
+
+        cars[0].getCarInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/day33_Custom_Class/_03_CarObjects2.java
@@ -0,0 +1,47 @@
+package day33_Custom_Class;
+
+public class _03_CarObjects2 {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        car1.setCarInfo("BMW","X5",2020,"BLACK");
+
+        car1.getCarInfo();
+
+        Car car2 = new Car();
+        car2.setCarInfo("Mercedes","S500",2020,"Red");
+
+        car2.getCarInfo();
+
+        Car car3 = new Car();
+        car3.setCarInfo("Audi","Q8",2020,"Gray");
+
+        car3.getCarInfo();
+
+        System.out.println("=======================================");
+
+        Car[] cars = {car1, car2, car3};
+
+        for (int i = 0; i <cars.length ; i++) {
+            cars[i].getCarInfo();
+
+        }
+
+        System.out.println("========================================");
+
+        for(Car eachCar: cars){
+
+            eachCar.getCarInfo();
+
+        }
+        System.out.println(car1); // toString methiod from the car gets executed.Otherwise we get hashCode
+
+
+    }
+}
