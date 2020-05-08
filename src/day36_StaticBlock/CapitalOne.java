799e0f0caed07012d422eb3d51fc3beb8d65bc91
Emre Yilmaz <emreyilmaz1992@outlook.com>
2020-05-08 10:45:43 -0400

    sdad

---
+++ src/day36_StaticBlock/CapitalOne.java
@@ -0,0 +1,17 @@
+package day36_StaticBlock;
+
+public class CapitalOne {
+
+    public static void main(String[] args) {
+
+        HumanResources.dev1.setInfo("Sia","Developer",10000,2323);
+
+        System.out.println(HumanResources.dev1);
+
+
+
+
+
+    }
+
+}
