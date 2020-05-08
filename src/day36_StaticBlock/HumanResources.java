9d465e6af3b24cd42e819f6794dc02080ac8c59a
Emre Yilmaz <emreyilmaz1992@outlook.com>
2020-05-08 10:43:55 -0400

    comment

--- src/day36_StaticBlock/HumanResources.java
+++ src/day36_StaticBlock/HumanResources.java
@@ -1,4 +1,27 @@
 package day36_StaticBlock;
 
+import day34_Custom_Class.Developer;
+
 public class HumanResources {
+    //dev1.setInfo("Namik","Developer",1200000,123);
+    static Developer dev1 = new Developer();
+    static Developer dev2 = new Developer();
+    static Developer dev3 = new Developer();
+    static Developer dev4 = new Developer();
+    static Developer dev5 = new Developer();
+
+
+
+
+
+
+
+
+
+    static{
+
+        dev1.setInfo("Namik","Developer",1200000,123);
+
+    }
+
 }
